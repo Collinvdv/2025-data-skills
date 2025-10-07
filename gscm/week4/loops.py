30c74512ac508ae5114d7f3ac64623b189a4dee3
Collin Van der Vorst <collinvandervorst@Mac.guests.thomasmore.be>
2025-10-07 10:26:09 +0200

    Added loops

--- gscm/week4/loops.py
+++ gscm/week4/loops.py
@@ -1,3 +1,49 @@
+# COUNT FROM 1 till 10
+for number in range(50, 101, 3):
+    print(number)
+
+
+for number in range(10, 0, -1):
+    print(number)
+
+
+# all the numbers that are divisible by 7 between 66 and 122
+# number % 7, if the remainder is 0 
+for number in range(66,123): 
+    if number % 7 == 0:
+        print(number)
+
+goats = ["T-rex", "Connie", "Clyde"]
+print(goats[0])
+print(goats[1])
+print(goats[2])
+
+for goat in goats:
+    print(goat)
+
+for goatIndex in range(0, len(goats)):
+    print(goats[goatIndex])
+
+grades = [8, 14, 20, 6]
+total = 0
+
+# calculate the average of grades 
+for grade in grades: 
+    total += grade
+
+avg = total / len(grades)
+
+print(avg)
+
+
+
+
+
+
+
+
+
+
 # Count from 1 to 100
 
 # Count from 50 to 100 
