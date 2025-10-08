fa0100f9da341f6cbfb54e2cebba8d1d5c0cbe91
Collin Van der Vorst <collinvandervorst@Mac.guests.thomasmore.be>
2025-10-08 12:17:54 +0200

    While assignment

--- bscm/week4/3_while.py
+++ bscm/week4/3_while.py
@@ -5,12 +5,13 @@ attempts = 1
 isBlocked = False
 
 while secretPassword != userPassword:
-    userPassword = input("Wroooong, give me password")
-    attempts += 1
-
     if attempts == 5:
         isBlocked = True
         break
+    
+    userPassword = input("Wroooong, give me password")
+    attempts += 1
+
 
 if isBlocked == True:
     print("Not welcome, user is blocked")
--- bscm/week4/4_while_assignment.py
+++ bscm/week4/4_while_assignment.py
@@ -1,4 +1,18 @@
-# The computer chooses a secret number between 1 and 10.
+# The computer chooses a secret number between 1 and 100.
 # The user has to guess until it’s correct.
 # After each wrong guess → print "Lower.Try again!" OR Higher.
 # When correct → print "Well done!".
+
+secretnumber = 55
+
+usernumber = int(input("What is the number?"))
+
+while secretnumber != usernumber:
+    if usernumber > secretnumber:
+        print("Lager")
+    else:
+        print("Hoger")
+    
+    usernumber = int(input("What is the number?"))
+
+print("Well done!")
